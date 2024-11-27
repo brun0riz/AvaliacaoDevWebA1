2d384c5b84ceeba78eaa14985d7720b105f9c9e8
brun0riz <btrizztto@gmail.com>
2024-11-27 11:56:07 -0300

    Avaliação bruno trevizan rizzatto

---
+++ API/API/Program.cs
@@ -0,0 +1,105 @@
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDataContext>();
+
+builder.Services.AddCors(options =>
+    options.AddPolicy("Acesso Total",
+    configs => configs
+        .AllowAnyHeader()
+        .AllowAnyOrigin()
+        .AllowAnyMethod()
+        )
+);
+
+var app = builder.Build();
+
+
+app.MapGet("/", () => "Avaliação A1");
+
+//ENDPOINTS DE CATEGORIA
+//GET: http://localhost:5273/api/categoria/listar
+app.MapGet("/api/categoria/listar", ([FromServices] AppDataContext ctx) =>
+{
+    if (ctx.Categorias.Any())
+    {
+        return Results.Ok(ctx.Categorias.ToList());
+    }
+    return Results.NotFound("Nenhuma categoria encontrada");
+});
+
+//POST: http://localhost:5273/api/categoria/cadastrar
+app.MapPost("/api/categoria/cadastrar", ([FromServices] AppDataContext ctx, [FromBody] Categoria categoria) =>
+{
+    ctx.Categorias.Add(categoria);
+    ctx.SaveChanges();
+    return Results.Created("", categoria);
+});
+
+//ENDPOINTS DE TAREFA
+//GET: http://localhost:5273/api/tarefas/listar
+app.MapGet("/api/tarefas/listar", ([FromServices] AppDataContext ctx) =>
+{
+    if (ctx.Tarefas.Any())
+    {
+        return Results.Ok(ctx.Tarefas.Include(x => x.Categoria).ToList());
+    }
+    return Results.NotFound("Nenhuma tarefa encontrada");
+});
+
+//POST: http://localhost:5273/api/tarefas/cadastrar
+app.MapPost("/api/tarefas/cadastrar", ([FromServices] AppDataContext ctx, [FromBody] Tarefa tarefa) =>
+{
+    Categoria? categoria = ctx.Categorias.Find(tarefa.CategoriaId);
+    if (categoria == null)
+    {
+        return Results.NotFound("Categoria não encontrada");
+    }
+    tarefa.Categoria = categoria;
+    ctx.Tarefas.Add(tarefa);
+    ctx.SaveChanges();
+    return Results.Created("", tarefa);
+});
+
+//PUT: http://localhost:5273/tarefas/alterar/{id}
+app.MapPut("/api/tarefas/alterar/{id}", ([FromServices] AppDataContext ctx, [FromRoute] string id, [FromBody] Tarefa tarefaAlterada) =>
+{
+    Tarefa? tarefa = ctx.Tarefas.Find(id);
+    if (tarefa == null)
+    {
+        return Results.NotFound();
+    }
+    Categoria? categoria = ctx.Categorias.Find(tarefa.CategoriaId);
+    if (categoria is null)
+    {
+        return Results.NotFound();
+    }
+    tarefa.Categoria = categoria;
+    tarefa.Titulo = tarefaAlterada.Titulo;
+    tarefa.Status = tarefaAlterada.Status;
+    tarefa.Descricao = tarefaAlterada.Descricao;
+    ctx.SaveChanges();
+    return Results.Ok(tarefa);
+});
+
+//GET: http://localhost:5273/tarefas/naoconcluidas
+app.MapGet("/api/tarefas/naoconcluidas", ([FromServices] AppDataContext ctx) =>
+{
+    
+});
+
+//GET: http://localhost:5273/tarefas/concluidas
+app.MapGet("/api/tarefas/concluidas", ([FromServices] AppDataContext ctx) =>
+{
+    if (ctx.Tarefas.Any())
+    {
+        return Results.Ok(ctx.Tarefas.Include(x => x.Categoria).ToList());
+    }
+    return Results.NotFound("Nenhuma tarefa encontrada");
+});
+
+app.UseCors("Acesso Total");
+
+app.Run();
